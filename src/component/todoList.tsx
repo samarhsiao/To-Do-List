e62b35e81d052af0811f61e5e95e86497855f738
Samar <s99502042@gmail.com>
2023-04-03 12:45:40 +0800

    feat:errMessage

--- src/component/todoList.tsx
+++ src/component/todoList.tsx
@@ -1,18 +1,36 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { AiOutlineCheck } from 'react-icons/ai';
 import { CiTrash } from "react-icons/ci";
-import { Fade } from "react-awesome-reveal";
+import {RiErrorWarningLine} from 'react-icons/ri' 
+import axios from 'axios';
+//import { Fade } from "react-awesome-reveal";
 const ToDoList = () => {
   const [today, setToday] = useState('');
   const [isChecked, setIsChecked] = useState(false);
   const [isDeleted, setIsDeleted] = useState(false);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const [errorMessage,setErrorMessage] = useState(false);
   useEffect(() => {
     const timestamp = Date.now();
     const timeObj = new Date(timestamp);
     const dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     let index = +timeObj.getDay();
     setToday(dayArray[index]);
+
   }, []);
+
+  const addToList = () => {
+      if(!inputRef.current?.value){
+        setErrorMessage(true);
+      }    
+  }
+   
+      
+
+
+
+
+
 
 
   return (
@@ -22,37 +40,36 @@ const ToDoList = () => {
         <div className="col-12">
           <div className="today">&#x1F9AD;{` Happy ${today} !`}</div>
           <ul className="todo-list ui-sortable">
-            
-              {!isDeleted  && <li className="col-8 col-md-9">
-                <div className="checkbox" onClick={() => {
-                  setIsChecked((val) => !val);
-                }}>
-                  {isChecked && <AiOutlineCheck style={{ position: "absolute" }} />}
-                  <input type="checkbox" />
-                  <label>
-                    <span className="checkbox-mask">
-                    </span><span className={isChecked ? "is-done" : ""}>take rest take resttake rest</span>
-                  </label>
-                </div>
-                <CiTrash style={{ width: "1.3em", height: "1.3em", margin: "0px 4px" }} onClick={()=>{
-                  setIsDeleted(true);
-                }}/>
-              </li>}
-           
+
+            {!isDeleted && <li className="col-8 col-md-9">
+              <div className="checkbox" onClick={() => {
+                setIsChecked((val) => !val);
+              }}>
+                {isChecked && <AiOutlineCheck style={{ position: "absolute" }} />}
+                <input type="checkbox" />
+                <label>
+                  <span className="checkbox-mask">
+                  </span><span className={isChecked ? "is-done" : ""}>take rest take resttake rest</span>
+                </label>
+              </div>
+              <CiTrash style={{ width: "1.3em", height: "1.3em", margin: "0px 4px" }} onClick={() => {
+                setIsDeleted(true);
+              }} />
+            </li>}
+
           </ul>
           <div className="add-control">
             <div className="form-group d-flex">
               <span className="add-icon"></span>
-              <textarea rows={3} className="add-item" placeholder="Enter task,ideas..." />
-              <div className="add-button">Add</div>
+              <textarea rows={3} className="add-item" placeholder="Enter task,ideas..." ref={inputRef} onChange={()=>{
+                setErrorMessage(false);
+              }}/>
+              <div className="add-button" onClick={() => {
+                addToList();
+              }}>Add</div>
             </div>
           </div>
-          {/* <div className="buttonWrapper">
-                <span className="add-icon d-none"></span>
-                 
-             </div>  */}
-          {/* 錯誤提示  */}
-          {/* <p className="err text-danger text-center hidden"><i className="fa fa-warning"></i> Oops! Please, enter name item</p>  */}
+          {errorMessage && <div className="err text-danger"> <RiErrorWarningLine style={{width: "1.1em", height: "1.1em"}}/>&nbsp;Oops! Please, enter name item</div> }
         </div>
 
 
