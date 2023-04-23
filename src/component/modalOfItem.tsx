9388fe5f30f7f77cc9eec90e64c534812dfabcfa
Samar <s99502042@gmail.com>
2023-04-23 13:47:29 +0800

    modify:add css style to modal

--- src/component/modalOfItem.tsx
+++ src/component/modalOfItem.tsx
@@ -1,6 +1,6 @@
-import {ToDoItem } from '../types/toDoItem';
-
+import { ToDoItem } from '../types/toDoItem';
 import { Button, Modal } from 'react-bootstrap';
+import { BsCheckCircleFill, BsHammer } from 'react-icons/bs';
 
 type Props = {
   modalShow: boolean;
@@ -19,15 +19,50 @@ export default function ModalOfItem({ modalShow, setModalShow, item }: Props) {
       >
         <Modal.Body>
           {/* <h4>{item.title}</h4> */}
-          <p>ToDo: {item.title}</p>
-          <p>Created Date: {item.createdAt?.slice(0,10).replace('-',' / ').replace('-',' / ')}</p>
-          <p>Updated Date: {item.updatedAt?.slice(0,10).replace('-',' / ').replace('-',' / ')}</p>
+          <div className="modal-text">
+            <p className="col-6">
+              <strong>ToDo:</strong>&nbsp;<span>{item.title}</span>
+            </p>
+            <p>
+              <strong>Status:</strong>&nbsp;
+              <span>
+                {item.isDone ? (
+                  <BsCheckCircleFill
+                    style={{ fill: '#43A047', width: '1.1em', height: '1.1em' }}
+                  />
+                ) : (
+                  <BsHammer
+                    style={{ fill: '#FFC007', width: '1.1em', height: '1.1em' }}
+                  />
+                )}
+              </span>
+            </p>
+          </div>
+          <p>
+            <strong>Created date:</strong>&nbsp;
+            <span>
+              {item.createdAt
+                ?.slice(0, 10)
+                .replace('-', ' / ')
+                .replace('-', ' / ')}
+            </span>
+          </p>
+          <p>
+            <strong>Last update:</strong>&nbsp;
+            <span>
+              {item.updatedAt
+                ?.slice(0, 10)
+                .replace('-', ' / ')
+                .replace('-', ' / ')}
+            </span>
+          </p>
         </Modal.Body>
         <Modal.Footer>
           <Button
             onClick={() => {
               setModalShow(false);
             }}
+            style={{ backgroundColor: 'rgb(6, 13, 8)' }}
           >
             Close
           </Button>
